43ab15b2f6795ac924f0b4d58f9783785243e144
Vyacheslav Voronenko <git@voronenko.info>
2019-05-22 16:54:29 +0200

    improve(build) adjusted environment

--- build-env.sh
+++ build-env.sh
@@ -1,9 +1,12 @@
 # use with source
+export GL_URL=github.com/rb3ckers
+export GO_PROJECT_NAMESPACE=trafficmirror
 if [ ! -d $(pwd)/.go ]
 then
-mkdir -p $(pwd)/.go/src/github.com/rb3ckers
-ln -s $(pwd) $(pwd)/.go/src/github.com/rb3ckers/trafficmirror
+mkdir -p $(pwd)/.go/src/$GL_URL
+ln -s $(pwd) $(pwd)/.go/src/$GL_URL/$GO_PROJECT
 fi
 export GOPATH=$(pwd)/.go
 export GOBIN=$GOPATH/bin
 export PATH=$GOBIN:$PATH
+export GO_PROJECT_PATH=$(pwd)/.go/src/$GL_URL/$GO_PROJECT
